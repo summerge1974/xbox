05d977a3ff321fe8de339ef42fe186cfded3c720
ling <geling1974@gmail.com>
2018-02-25 12:45:57 +0800

    chang code

--- common/models/xbox.js
+++ common/models/xbox.js
@@ -146,7 +146,7 @@ module.exports = function(Xbox) {
         }
 
         var _deviceId = "12345678";
-        if ( !_.isUndefined(deviceInfo)){
+        if ( !_.isUndefined(deviceInfo.deviceId)){
             _deviceId = deviceInfo.deviceId;
         }
 
@@ -155,11 +155,11 @@ module.exports = function(Xbox) {
         var _userInfo = {};
         ps.push(ExecuteSyncSQLResult(bsSQL, _userInfo));
 
-        bsSQL = "SELECT a.deviceId,a.cageId,a.bookId,b.categoryId,b.title,b.image,now() as startDate, date_add(now(), interval b.leaseDays day) as endDate FROM xb_devicebooks a, xb_books b where a.bookid = b.bookid and a.deviceId like '%" + deviceInfo.deviceId + "%' order by a.cageId";
+        bsSQL = "SELECT a.deviceId,a.cageId,a.bookId,b.categoryId,b.title,b.image,now() as startDate, date_add(now(), interval b.leaseDays day) as endDate FROM xb_devicebooks a, xb_books b where a.bookid = b.bookid and a.deviceId like '%" + _deviceId + "%' order by a.cageId";
         var _booksList = {};
         ps.push(ExecuteSyncSQLResult(bsSQL, _booksList));
 
-        bsSQL = "select a.categorieId as id, a.categoriename as name from xb_categories a where a.categorieId in (select categorieId from xb_devicebooks where deviceID like '%" + _deviceId + "%' ";
+        bsSQL = "select a.categorieId as id, a.categoriename as name from xb_categories a where a.categorieId in (select categorieId from xb_devicebooks where deviceID like '%" + _deviceId + "%') ";
         var _bookcategories = {};
         ps.push(ExecuteSyncSQLResult(bsSQL, _bookcategories));
 
