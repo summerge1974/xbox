2fddc3e8ebd2f09283e8fee2ab0606aadddca85c
summerge1974 <geling1974@gmail.com>
2018-04-17 13:24:52 +0800

    chang code

--- common/models/we-chat-event.js
+++ common/models/we-chat-event.js
@@ -71,8 +71,8 @@ module.exports = function(Wechatevent) {
 
         EWTRACE("ValidateWechatEvent Begin")
         console.log(req.body.xml);
-        conosle.log(req.queyr);
-        
+        console.log(req.queyr);
+
         var q = req.query;
         var openid = q.openid; //微信加密签名  
 
