29674b7ea7a664a90b199723ab8befefcd48545e
summerge1974 <geling1974@gmail.com>
2018-05-25 17:54:02 +0800

    chang code

--- server/ssl-config.js
+++ server/ssl-config.js
@@ -1,4 +1,4 @@
 var path = require('path'),
 fs = require("fs");
-exports.privateKey = fs.readFileSync(path.join(__dirname, './private/maomaochong.key')).toString();
-exports.certificate = fs.readFileSync(path.join(__dirname, './private/maomaochong.pem')).toString();
+exports.privateKey = fs.readFileSync(path.join(__dirname, './private/style.man-kang.key')).toString();
+exports.certificate = fs.readFileSync(path.join(__dirname, './private/style.man-kang.pem')).toString();
