76efc3c00b7a8039b5128e58e3b985e33b2d7a15
葛岭 <geling1974@gmail.com>
2018-07-15 16:58:57 +0800

    chang code

--- common/models/xbox.js
+++ common/models/xbox.js
@@ -1587,7 +1587,7 @@ module.exports = function(Xbox) {
     s = s * 6378.137; // EARTH_RADIUS;
     s = Math.round(s * 10000) / 10000; //输出为公里
     //s=s.toFixed(4);
-    return s;
+    return s*1.6;
   }
 
   Xbox.isNearDevice = function(lbsInfo, cb) {
